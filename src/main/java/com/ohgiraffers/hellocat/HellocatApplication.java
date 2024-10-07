16e42d19184b92a083c50d0d3672aa3906d09ded
Cheong Ye Jun <jjyejun@naver.com>
2024-10-07 17:53:56 +0900

    프로젝트 생성

---
+++ src/main/java/com/ohgiraffers/hellocat/HellocatApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.hellocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellocatApplication.class, args);
+    }
+
+}
