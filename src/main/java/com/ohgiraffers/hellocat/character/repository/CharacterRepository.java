0ee5f3243be010e5b91c2954ecf7ada6691dfacb
Cheong Ye Jun <jjyejun@naver.com>
2024-10-21 15:04:55 +0900

    Refactor: 캐릭터 리포지토리 반환값 변경
    - findByUserId의 반환값을 Character -> Optional<Character>로 변경

--- src/main/java/com/ohgiraffers/hellocat/character/repository/CharacterRepository.java
+++ src/main/java/com/ohgiraffers/hellocat/character/repository/CharacterRepository.java
@@ -3,7 +3,9 @@ package com.ohgiraffers.hellocat.character.repository;
 import com.ohgiraffers.hellocat.character.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CharacterRepository extends JpaRepository<Character, Long> {
 
-    Character findByUserId(Long userId);
+    Optional<Character> findByUserId(Long userId);
 }
