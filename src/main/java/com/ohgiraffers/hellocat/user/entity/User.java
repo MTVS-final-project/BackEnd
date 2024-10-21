990ef76ccfa74edbd06564236c79f80fb694652d
Cheong Ye Jun <jjyejun@naver.com>
2024-10-21 16:55:45 +0900

    Refactor: 유저 엔티티 리팩토링 (User)
    - NoArgsConstructor 사용
    - 기존의 생성자에서 생성하던 로직을 새로운 편의 메서드로 변경 -> 유저가 생성된 후 유저를 통해 캐릭터가 생성되는 로직으로 변경

--- src/main/java/com/ohgiraffers/hellocat/user/entity/User.java
+++ src/main/java/com/ohgiraffers/hellocat/user/entity/User.java
@@ -3,23 +3,24 @@ package com.ohgiraffers.hellocat.user.entity;
 import com.ohgiraffers.hellocat.character.entity.Character;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.CascadeType.*;
 import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    
     @OneToOne(mappedBy = "user", cascade = ALL, fetch = LAZY)
     private Character character;
 
-    public User() {
+    public void createCharacter() {
         this.character = new Character(this);
     }
 }
