35d1c411cc82d379c1fbc62298723b557ba24fed
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-10-22 17:20:34 +0900

    Refactor: 캐릭터 리포지토리 변경
    - 캐릭터 엔티티 UserId를 갖지 않게 되며 해당 값을 찾는 메서드 삭제 (findByUserId)

--- src/main/java/com/ohgiraffers/hellocat/character/repository/CharacterRepository.java
+++ src/main/java/com/ohgiraffers/hellocat/character/repository/CharacterRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
-
-    Optional<Character> findByUserId(Long userId);
 }
