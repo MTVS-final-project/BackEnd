e8eb73ab1e82094126d85e0dc0486d8e5d44c643
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-10-24 00:35:03 +0900

    Style: 유저 컨트롤러 구분을 위한 공백 추가

--- src/main/java/com/ohgiraffers/hellocat/user/controller/UserController.java
+++ src/main/java/com/ohgiraffers/hellocat/user/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
 
         Character character = new Character();
         characterService.createCharacter(character);
-        
+
         user.makeCharacter(character);
 
         UserCreateResponseDto responseDto = userService.createUser(user);
