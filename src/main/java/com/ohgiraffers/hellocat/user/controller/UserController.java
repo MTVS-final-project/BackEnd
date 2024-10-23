f1ad1a913ad8a4dbfcc9d7756aa889983ac7f6c2
Cheong Ye Jun <jjyejun@naver.com>
2024-10-23 11:26:04 +0900

    Feat: 유저 생성 로직 구현 (Controller, Service, ResponseDto)
    - 유저 생성용 컨트롤러 매핑
    - 유저 생성용 서비스 구현 -> UserCreateResponseDto 타입 반환
    - 유저 생성 응답용 DTO 구현

--- src/main/java/com/ohgiraffers/hellocat/user/controller/UserController.java
+++ src/main/java/com/ohgiraffers/hellocat/user/controller/UserController.java
@@ -1,11 +1,42 @@
 package com.ohgiraffers.hellocat.user.controller;
 
+import com.ohgiraffers.hellocat.character.entity.Character;
+import com.ohgiraffers.hellocat.user.dto.UserCreateResponseDto;
+import com.ohgiraffers.hellocat.user.entity.User;
+import com.ohgiraffers.hellocat.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 @Tag(name = "유저 API", description = "유저와 관련한 API")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @Operation(summary = "유저 생성", description = "유저를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "유저가 정상 생성되었습니다.")
+    })
+    public ResponseEntity<?> createUser() {
+
+        User user = new User();
+        Character character = new Character();
+        user.makeCharacter(character);
+
+        UserCreateResponseDto responseDto = userService.createUser(user);
+
+        return ResponseEntity.status(201).body(responseDto);
+    }
 }
