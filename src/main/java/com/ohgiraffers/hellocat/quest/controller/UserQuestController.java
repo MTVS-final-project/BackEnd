ae6384a6431a3f92df9baba0a2600369b7afbcca
Cheong Ye Jun <jjyejun@naver.com>
2024-10-29 11:13:12 +0900

    Fix: 유저 퀘스트 컨트롤러 응답 상태 코드 수정

--- src/main/java/com/ohgiraffers/hellocat/quest/controller/UserQuestController.java
+++ src/main/java/com/ohgiraffers/hellocat/quest/controller/UserQuestController.java
@@ -90,7 +90,7 @@ public class UserQuestController {
     @DeleteMapping("/{questId}")
     @Operation(summary = "유저 퀘스트 삭제", description = "유저가 퀘스트를 삭제합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "퀘스트가 정상적으로 삭제되었습니다."),
+            @ApiResponse(responseCode = "204", description = "퀘스트가 정상적으로 삭제되었습니다."),
             @ApiResponse(responseCode = "403", description = "퀘스트의 상태로 인해 삭제할 수 없습니다."),
             @ApiResponse(responseCode = "404", description = "퀘스트를 찾을 수 없습니다."),
     })
