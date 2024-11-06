2a1c0ba3f00caae7da43cf818f4a5abcbc542d49
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-11-07 03:16:25 +0900

    Feat: 공지 컨트롤러 생성(유저/어드민)

---
+++ src/main/java/com/ohgiraffers/hellocat/notice/controller/NoticeAdminController.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.hellocat.notice.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "공지/이벤트 API", description = "공지 및 이벤트 관련 API")
+@Slf4j
+@RestController
+@RequestMapping("/admin/api/v1/notice")
+@RequiredArgsConstructor
+public class NoticeAdminController {
+}
