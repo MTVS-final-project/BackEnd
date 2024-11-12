cb742dea2ce50c2fefda327a9e9d3dca195de54a
Cheong Ye Jun <jjyejun@naver.com>
2024-11-12 10:15:19 +0900

    Feat: 아이템 리포지토리 구현

---
+++ src/main/java/com/ohgiraffers/hellocat/market/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.hellocat.market.repository;
+
+import com.ohgiraffers.hellocat.item.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
