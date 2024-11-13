2582a474850855fe8ad2a7b3b3bbadef2186e36c
Cheong Ye Jun <jjyejun@naver.com>
2024-11-13 09:52:08 +0900

    Feat (Request/Response Dto, Service): 유저 아이템 생성용 메서드 구현
    - 마켓 트레이드 과정에서 사용하는 유저에게 아이템 추가하는 메서드 구현
    
    개수 로직 변경 필요

--- src/main/java/com/ohgiraffers/hellocat/item/dto/ItemRequestDto.java
+++ src/main/java/com/ohgiraffers/hellocat/item/dto/ItemRequestDto.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.hellocat.item.dto;
 
+import com.ohgiraffers.hellocat.market.entity.MarketItem;
 import com.ohgiraffers.hellocat.market.enums.MarketItemCategory;
 import com.ohgiraffers.hellocat.user.entity.User;
 import lombok.AllArgsConstructor;
@@ -17,4 +18,14 @@ public class ItemRequestDto {
     private String description;
     private Long count;
     private MarketItemCategory category;
+
+    public ItemRequestDto(MarketItem foundItem, User buyerId) {
+        this.userId = buyerId;
+        this.makerId = foundItem.getMakerId();
+        this.name = foundItem.getName();
+        this.description = foundItem.getDescription();
+        this.count = 1L;
+        // Todo: 개수 로직 추후 변경
+        this.category = foundItem.getCategory();
+    }
 }
