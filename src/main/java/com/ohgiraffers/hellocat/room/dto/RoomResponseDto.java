a0545d2093eb136560b5cf291ecb31d6eacb43f5
Cheong Ye Jun <jjyejun@naver.com>
2024-11-20 15:12:59 +0900

    Feat: 룸 저장 응답 DTO 가구 리스트 추가

--- src/main/java/com/ohgiraffers/hellocat/room/dto/RoomResponseDto.java
+++ src/main/java/com/ohgiraffers/hellocat/room/dto/RoomResponseDto.java
@@ -1,9 +1,12 @@
 package com.ohgiraffers.hellocat.room.dto;
 
+import com.ohgiraffers.hellocat.room.entity.Furniture;
 import com.ohgiraffers.hellocat.room.entity.Room;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -12,9 +15,11 @@ public class RoomResponseDto {
 
     private String id;
     private Long makerId;
+    private List<Furniture> furnitureList;
 
     public RoomResponseDto(Room room) {
         this.id = room.getId();
         this.makerId = room.getMakerId();
+        this.furnitureList = room.getFurnitureList();
     }
 }
