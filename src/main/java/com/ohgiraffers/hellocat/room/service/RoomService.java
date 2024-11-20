011abcada3b5b0aede4b39fca47012ace92ecf7f
Cheong Ye Jun <jjyejun@naver.com>
2024-11-20 15:12:09 +0900

    Feat: 가구 저장 서비스 가구 DTO 변환 로직 추가

--- src/main/java/com/ohgiraffers/hellocat/room/service/RoomService.java
+++ src/main/java/com/ohgiraffers/hellocat/room/service/RoomService.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.hellocat.room.service;
 
 import com.ohgiraffers.hellocat.room.dto.RoomRequestDto;
 import com.ohgiraffers.hellocat.room.dto.RoomResponseDto;
+import com.ohgiraffers.hellocat.room.entity.Furniture;
 import com.ohgiraffers.hellocat.room.entity.Room;
 import com.ohgiraffers.hellocat.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class RoomService {
 
     private final RoomRepository roomRepository;
 
-    public RoomResponseDto saveRoom(RoomRequestDto roomRequestDto) {
+    public RoomResponseDto saveRoom(RoomRequestDto requestDto) {
 
-        Room room = new Room(roomRequestDto);
+        List<Furniture> furnitureList = requestDto.getFurnitureList().stream()
+                .map(Furniture::new)
+                .toList();
+
+        Room room = new Room(requestDto, furnitureList);
 
         roomRepository.save(room);
 
