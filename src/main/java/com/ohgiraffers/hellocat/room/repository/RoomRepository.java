a40a84519596eaee7fc340be40bc3032dd8e8472
Cheong Ye Jun <jjyejun@naver.com>
2024-11-25 14:17:57 +0900

    Feat: 룸 리포지토리 생성자 기반 조회 기능 구현

--- src/main/java/com/ohgiraffers/hellocat/room/repository/RoomRepository.java
+++ src/main/java/com/ohgiraffers/hellocat/room/repository/RoomRepository.java
@@ -3,5 +3,9 @@ package com.ohgiraffers.hellocat.room.repository;
 import com.ohgiraffers.hellocat.room.entity.Room;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends MongoRepository<Room, String> {
+
+    Optional<Room> findByMakerId(Long makerId);
 }
