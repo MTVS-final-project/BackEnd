3ada1b57a991047cf715fa3f34d428f16d33cab9
Cheong Ye Jun <113380447+LooanCheong@users.noreply.github.com>
2024-11-26 21:57:43 +0900

    Fix: 마켓 룸 리포지토리 extends 오류 수정

--- src/main/java/com/ohgiraffers/hellocat/market/repository/MarketRoomRepository.java
+++ src/main/java/com/ohgiraffers/hellocat/market/repository/MarketRoomRepository.java
@@ -1,7 +1,7 @@
 package com.ohgiraffers.hellocat.market.repository;
 
 import com.ohgiraffers.hellocat.market.entity.MarketRoom;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MarketRoomRepository extends CrudRepository<MarketRoom, Long> {
+public interface MarketRoomRepository extends MongoRepository<MarketRoom, Long> {
 }
