08cdf23b2007b6aa6727354cf76d1c6810830e53
Cheong Ye Jun <jjyejun@naver.com>
2024-11-26 13:28:01 +0900

    Feat: 마켓 룸 리포지토리 생성

---
+++ src/main/java/com/ohgiraffers/hellocat/market/repository/MarketRoomRepository.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.hellocat.market.repository;
+
+import com.ohgiraffers.hellocat.market.entity.MarketRoom;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarketRoomRepository extends CrudRepository<MarketRoom, Long> {
+}
