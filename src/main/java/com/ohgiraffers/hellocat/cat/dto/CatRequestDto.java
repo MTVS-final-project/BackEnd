fcba4e597cf38b3309724187b31bc1e8e36e1e5a
Cheong Ye Jun <jjyejun@naver.com>
2024-11-27 18:02:04 +0900

    Feat (RequestDto, UpdateRequestDto, Entity): Vaild 조건 추가

--- src/main/java/com/ohgiraffers/hellocat/cat/dto/CatRequestDto.java
+++ src/main/java/com/ohgiraffers/hellocat/cat/dto/CatRequestDto.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.hellocat.cat.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,35 +11,51 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CatRequestDto {
 
+    @NotNull(message = "고양이 주인의 아이디는 필수입니다.")
     private Long ownerId;
 
+    @NotEmpty(message = "고양이의 성격은 필수입니다.")
     private String personality;
 
+    @NotNull(message = "고양이의 나이는 필수입니다.")
     private Double age;
 
+    @NotNull(message = "고양이의 몸무게는 필수입니다.")
     private Double weight;
 
+    @NotNull(message = "고양이의 성별은 필수입니다.")
     private Boolean male;
 
+    @NotNull(message = "고양이의 졸음 수치는 필수입니다.")
     private Double sleepy;
 
+    @NotNull(message = "고양이의 배고픔 수치는 필수입니다.")
     private Double hunger;
 
+    @NotNull(message = "고양이의 친밀도는 필수입니다.")
     private Double friendly;
 
+    @NotNull(message = "고양이의 기분은 필수입니다.")
     private Double mood;
 
+    @NotNull(message = "고양이의 신진대사는 필수입니다.")
     private Double metabolism;
 
+    @NotNull(message = "고양이의 이동 간격은 필수입니다.")
     private Double moveTerm;
 
+    @NotNull(message = "고양이의 이동 범위는 필수입니다.")
     private Double moveRange;
 
+    @NotNull(message = "고양이의 배설 욕구는 필수입니다.")
     private Double discharge;
 
+    @NotNull(message = "고양이의 속도는 필수입니다.")
     private Double speed;
 
+    @NotNull(message = "고양이의 식사 속도는 필수입니다.")
     private Double eatSpeed;
 
+    @NotNull(message = "고양이의 인덱스는 필수입니다.")
     private Double catIndex;
 }
